3a1d8a8c700f8831db94def1dbcf6978c005f658
aswarina <lyxsyyawa@gmail.com>
2026-03-03 22:04:07 +0700

    menambahkan fungsi jumlah dan luaspersegip

--- file3.cpp
+++ file3.cpp
@@ -11,5 +11,12 @@ void input() {
     cin >> p;
     cout << "Masukkan lebar = ";
     cin >> l;
+}
 
+float jumlah(float a, float b, float c){
+    return a + b +c;
+}
+
+int luaspersegip(int a, int b){
+    return a * b;
 }
